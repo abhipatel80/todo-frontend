9bd2ef7dd9d53809f8c082aa3fcac4fde4d8d39a
Abhi Patel <abhipatel6788@gmail.com>
2023-10-31 12:27:08 +0530

    change api-path

--- src/hooks/fetch.js
+++ src/hooks/fetch.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 const useFetch = (path, body) => {
 
+    const url = "https://todo-backend-flame.vercel.app";
+
     const [todo, setTodo] = useState();
 
     const addTodo = async () => {
         try {
-            const { data } = await axios.post(`http://localhost:4000/todo${path}`, body);
+            const { data } = await axios.post(`${url}/todo${path}`, body);
             return data;
         } catch (e) {
             console.log(e.message);
@@ -16,7 +18,7 @@ const useFetch = (path, body) => {
 
     const getTodo = async () => {
         try {
-            const { data } = await axios.get(`http://localhost:4000/todo${path}`);
+            const { data } = await axios.get(`${url}/todo${path}`);
             setTodo(data);
         } catch (e) {
             console.log(e.message);
@@ -25,7 +27,7 @@ const useFetch = (path, body) => {
 
     const getTodoById = async (id) => {
         try {
-            const { data } = await axios.get(`http://localhost:4000/todo${path}/${id}`);
+            const { data } = await axios.get(`${url}/todo${path}/${id}`);
             setTodo(data);
         } catch (e) {
             console.log(e.message);
@@ -34,7 +36,7 @@ const useFetch = (path, body) => {
 
     const editTodo = async (id) => {
         try {
-            const { data } = await axios.put(`http://localhost:4000/todo${path}/${id}`, body);
+            const { data } = await axios.put(`${url}/todo${path}/${id}`, body);
             setTodo(data);
         } catch (e) {
             console.log(e.message);
@@ -43,7 +45,7 @@ const useFetch = (path, body) => {
 
     const deleteTodo = async (id) => {
         try {
-            await axios.delete(`http://localhost:4000/todo${path}/${id}`);
+            await axios.delete(`${url}/todo${path}/${id}`);
         } catch (e) {
             console.log(e.message);
         }
